903180d5805cec63b402938c530f736e6ae65c43
Zachary Farrell <zfarrell0507@gmail.com>
2020-06-27 18:03:47 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,7 +2,7 @@ def second_challenge
 epic_tragedy = {
   :montague => {
       :patriarch => {:name => "Lord Montague", :age => "53"},
-      :matriarch => {},
+      :matriarch => {:name => "Lady Montague", :age => "54"},
       :hero => {},
       :hero_friends => []
   },
