077166970bb3328cd54804ef6c4d2bf1c87b9efb
Zachary Farrell <zfarrell0507@gmail.com>
2020-06-27 18:00:08 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,8 +1,11 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      
-      }, 
+      :patriarch => {}
+      :matriarch => {}
+      :hero => {}
+      :hero_friends => []
+      },
     :capulet => {
       
       }}
